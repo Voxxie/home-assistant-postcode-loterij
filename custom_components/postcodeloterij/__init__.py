9642a144349bbaf154ebe7901437d3a2f37ac05b
Hans Vos <hansvos@ingestel.nl>
2026-01-17 22:04:35 +0100

    fix for bug

--- custom_components/postcodeloterij/__init__.py
+++ custom_components/postcodeloterij/__init__.py
@@ -1,20 +1,25 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, CONF_POSTCODE
+from .const import DOMAIN
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Postcodeloterij from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Nieuwe HA API: lijst van platforms
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+
     return unload_ok
